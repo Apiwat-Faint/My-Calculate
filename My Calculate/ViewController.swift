9ee6a829cd5f878d8fcab6e232323bb6fc495449
apiwat wattanasura <apiwatwattanasura@apiwats-MacBook-Pro.local>
2017-06-13 10:26:34 +0700

    PrintShowLog

--- My Calculate/ViewController.swift
+++ My Calculate/ViewController.swift
@@ -9,11 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //Implicit
+    var intNumber1 = 0
+    var intNumber2: Int = 0
+    var strNumber1 = ""
+    var strNumber2: String = ""
+    
+    
+    
+    
+    @IBOutlet weak var Num1TextFieid: UITextField!
+    
+    @IBOutlet weak var Num2TextFieid: UITextField!
+    
+    
+    @IBAction func answerButton(_ sender: Any) {
+        
+        strNumber1 = Num1TextFieid.text!
+        strNumber2 = Num2TextFieid.text!
+        
+        
+        //Show Log
+        print("strNumber1 ==> \(strNumber1)"  + " strNumber2 ==> \(strNumber2)")
+        
+    } // answerButton
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-    }
+    } //Main Method
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -21,5 +47,5 @@ class ViewController: UIViewController {
     }
 
 
-}
+} //Main Class
 
